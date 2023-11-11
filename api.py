e316044d94bb015b5fb4a6e5a775fd6e2806fff8
Cyan Yan <cyan_cg@qq.com>
2023-11-11 18:16:35 +0800

    Amend

--- api.py
+++ api.py
@@ -12,16 +12,35 @@ def api_host_informations():
     print(
         f"Memory Usage: {used_memory:.2f} GB / {total_memory:.2f} GB ({memory_percentage}%)"
     )
+
     disk_usage = psutil.disk_usage("/")
     total_disk = disk_usage.total / (1024**3)
     used_disk = disk_usage.used / (1024**3)
     disk_percentage = disk_usage.percent
     print(f"Disk Usage: {used_disk:.2f} GB / {total_disk:.2f} GB ({disk_percentage}%)")
-    # System Uptime
+
     uptime_seconds = time.time() - psutil.boot_time()
     uptime_hours = uptime_seconds // 3600
     uptime_minutes = (uptime_seconds % 3600) // 60
     print(f"System Uptime: {int(uptime_hours)} hours, {int(uptime_minutes)} minutes")
+
+    # CPU Usage
+    cpu_usage = psutil.cpu_percent(interval=1)
+    print(f"CPU Usage: {cpu_usage}%")
+
+    # Logged-in Users
+    users = ",".join([user.name for user in psutil.users()])
+
+    # Battery Information
+    try:
+        battery = psutil.sensors_battery()
+        if battery:
+            battery_status = f"{battery.percent}% {'charging' if battery.power_plugged else 'not charging'}"
+        else:
+            battery_status = "No battery"
+    except AttributeError:
+        battery_status = "Not available"
+
     return [
         {"key": "Host Name", "value": str(os.name)},
         {"key": "System Type", "value": str(platform.system())},
@@ -33,7 +52,14 @@ def api_host_informations():
             "value": f"{used_memory:.2f} GB / {total_memory:.2f} GB ({memory_percentage}%)",
         },
         {
+            "key": "Disk Usage",
+            "value": f"{used_disk:.2f} GB / {total_disk:.2f} GB ({disk_percentage}%)",
+        },
+        {
             "key": "System Uptime",
             "value": f"{int(uptime_hours)} hours, {int(uptime_minutes)} minutes",
         },
+        {"key": "CPU Usage", "value": f"{cpu_usage}%"},
+        {"key": "Logged-in Users", "value": users},
+        {"key": "Battery Status", "value": battery_status},
     ]
