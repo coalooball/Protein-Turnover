7c66172198aff344938f736ee4810c6d019ce70e
Cyan Yan <cyan_cg@qq.com>
2023-11-11 15:07:14 +0800

    Switch the project to Vue3.

--- app.py
+++ app.py
@@ -1,20 +1,15 @@
 from flask import Flask, jsonify, send_from_directory
 
-app = Flask(__name__)
-
-app = Flask(__name__, static_folder='static')
+app = Flask(__name__, static_folder='dist', static_url_path="/vue")
 
 @app.route('/api/data')
 def get_data():
     return jsonify({"message": "Hello from Flask!"})
 
-@app.route('/')
-def index():
-    return send_from_directory(app.static_folder, 'index.html')
-
+@app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def static_proxy(path):
-    return send_from_directory(app.static_folder, path)
+    return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=9880)
